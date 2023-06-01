18970cc9b4c6a92170bf65b44783d476883cfbd0
Mohammed Elshora <elshoram974@gmail.com>
2023-06-01 09:15:43 +0300

    after all

--- lib/controller.dart
+++ lib/controller.dart
@@ -1,9 +1,22 @@
+import 'package:tagrba/model.dart';
 import 'package:tagrba/selectitem.dart';
 
-class MyHomeController extends ModelController{
+class MyHomeController extends ModelController {
   @override
   void onInit() {
-    modelList = 
+    modelList = MyModelName.list;
     super.onInit();
+  }
+
+  @override
+  void onTap(int selectedIndex) {
+    print("on tap aaaaaaaaaa");
+    super.onTap(selectedIndex);
+  }
+
+  @override
+  void onLongPress(int selectedIndex) {
+    print("on long tap aaaaaaaaaa");
+    super.onLongPress(selectedIndex);
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:tagrba/controller.dart';
+import 'package:tagrba/model.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,9 +23,29 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("List")),
-      body: ListView(),
-    );
+    return GetBuilder<MyHomeController>(
+        init: MyHomeController(),
+        builder: (controller) {
+          return WillPopScope(
+            onWillPop: controller.onWillPop,
+            child: Scaffold(
+              appBar: AppBar(title: const Text("List")),
+              body: ListView.builder(
+                itemCount: controller.modelList.length,
+                itemBuilder: (context, index) {
+                  MyModelName modelName = controller.modelList[index];
+                  return ListTile(
+                    onTap: () => controller.onTap(index),
+                    onLongPress: () => controller.onLongPress(index),
+                    leading: Text(modelName.id.toString()),
+                    title: Text(modelName.name),
+                    subtitle: Text(modelName.note.toString()),
+                    trailing: Text(modelName.dateTime!.year.toString()),
+                  );
+                },
+              ),
+            ),
+          );
+        });
   }
 }
